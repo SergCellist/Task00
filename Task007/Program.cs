d0d518a7768a86833a46bdab43731b6c31b5d6f5
SergeyCellist1991 <serg311091@gmail.com>
2023-09-18 22:39:15 +0300

    Добавили задачу на нахождение последней цифры из трёхзначного числа

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//7. Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает последнюю цифру
+//этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;                 // для работы с отрицательными числами
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10; //что б получить третью цифру,надо получить остаток от деления на 10
+    Console.WriteLine($"Последняя цифра чила -> {lastDigit}");
+}
+else {
+ Console.WriteLine("Некорректный ввод");   
+}
