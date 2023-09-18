aa5acbe13848f0fcafd0491e004b57d40a872282
SergeyCellist1991 <serg311091@gmail.com>
2023-09-18 21:26:54 +0300

    Добавили задачу на возведение в крвадрат

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// 1. на вход принимает число
+// 2. выдаёт его квадрат (число умножить само на себя)
+// 3. вывод результата
+// Например:
+// 4 => 16
+// -3 => 9
+// -7 => 49
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine(number + " => " + square);
+Console.WriteLine($"{number} -> {square}");
